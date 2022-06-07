d123ad236d01e9cc80f4eac176f76b3c704e9d6f
JoseLopez07 <a01283642@itesm.mx>
2022-06-07 16:27:25 -0500

    confirmar

--- src/routes/diccionario.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import {getPalabras} from '../controllers/diccionario.controller'
-
-const router = Router()
-
-router.get('/diccionario', getPalabras)
-
-export default router
